b55b0159c770eefcd544dc4aa09d37299278f64d
kmeraz2 <54649455+kmeraz2@users.noreply.github.com>
2019-09-29 15:05:13 -0700

    Add files via upload

---
+++ palind2.py
@@ -0,0 +1,10 @@
+def palind2(s):
+    alist=list(s)
+    alist.reverse()
+    revs="".join(alist)
+    if revs==s:
+        return True
+    return False
+
+print(palind2("ratsliveonnoevilstar"))
+print(palind2("abcdedvba"))
